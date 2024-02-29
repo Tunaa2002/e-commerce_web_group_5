112bf8dfd5c1ce53fdb959ccf970671f486b70e3
Tunaa <damthantuananh1202@gmail.com>
2024-02-29 21:50:30 +0700

    new

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App(){
+
+    return(
+        <div className='main'>
+
+        </div>
+    )
+}
+
+export default App;
