9da67ee97e99fac7174b8792ed5b2cb20f3164e1
CDHai <caoduyhai007@gmail.com>
2024-04-24 00:38:11 +0700

    updating api bone

--- RestAPI/app.py
+++ RestAPI/app.py
@@ -1,5 +1,41 @@
 from bookstore import create_app
+import mysql.connector
 
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
+
+connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='123456',
+    database='ecommerce'
+)
+
+@app.route('/api/books/<category_name>', methods=['GET'])
+def get_books_by_category(category_name):
+    try:
+        cursor = connection.cursor()
+
+        # Truy vấn MySQL để lấy dữ liệu các cuốn sách từ một danh mục cụ thể
+        query = """
+            SELECT b.book_id, b.title, b.author_id, b.producer_id, b.category_id, b.price, b.description
+            FROM books b
+            JOIN category c ON b.category_id = c.category_id
+            WHERE c.name = %s
+        """
+        cursor.execute(query, (category_name,))
+
+        # Lấy kết quả từ truy vấn
+        books = cursor.fetchall()
+
+        # Chuyển đổi kết quả thành định dạng JSON và trả về
+        return jsonify({'books': books})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+    finally:
+        # Đóng kết nối
+        if connection.is_connected():
+            cursor.close()
--- RestAPI/bookstore/__init__.py
+++ RestAPI/bookstore/__init__.py
@@ -1,18 +1,8 @@
 from flask import Flask, request, Blueprint
 from .book.controller import books
-from .extension import db
-from .model import Category
 import os
 
-def create_db(app):
-    if os.path.exists("/bookstore/bookstore.db"):
-        db.create_all(app = app)
-        print("Created DB!")
-
-def create_app(config_file = "config.py"):
+def create_app():
     app = Flask(__name__)
-    db.init_app(app)
-    app.config.from_pyfile(config_file)
-    create_db(app)
     app.register_blueprint(books)
     return app
